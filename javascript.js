f59da671a98b07197b104660d1a3789000e896e1
Gustavo Maigua <gustavo.maigua.fce@gmail.com>
2023-05-28 09:22:48 -0300

    Se añadio articulos, aside y buscador. Ademas se modifico el logo y se establecio las rutas correspondientes

--- javascript.js
+++ javascript.js
@@ -47,12 +47,6 @@ prevButton.addEventListener ('click', () => {
   goToSlide (index);
 });
 
-/*prevButton.addEventListener('click', () => {
-    let index = currentIndex - 1;
-    index = index < 0 ? carouselItems.length - 1 : index;
-    goToSlide(index);
-  });
-*/
 nextButton.addEventListener ('click', () => {
     let index = currentIndex + 1;
     if (index >= carouselItems.length) {
@@ -61,10 +55,78 @@ nextButton.addEventListener ('click', () => {
     goToSlide (index);
 });
 
-/*nextButton.addEventListener('click', () => {
-    let index = currentIndex + 1;
-    index = index >= carouselItems.length ? 0 : index;
-    goToSlide(index);
-  });
-*/
 intervalId = setInterval (autoAdvance, 5000);
+
+/*        search         */
+
+// Obtener referencia al formulario de búsqueda
+const searchForm = document.getElementById('searchForm');
+
+// Manejar el evento de envío del formulario
+searchForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Obtener el valor ingresado en el campo de búsqueda
+  const searchTerm = document.getElementById('searchInput').value;
+
+  // Redirigir a la página de resultados de búsqueda con el término de búsqueda como parámetro
+  window.location.href = `paginas/catalogo/catalogo.html?search=${encodeURIComponent(searchTerm)}`;
+});
+
+
+
+
+/*       calendario        */
+
+// Obtener la referencia a los elementos con las clases "month" y "year"
+const monthElement = document.querySelector(".month");
+const yearElement = document.querySelector(".year");
+
+// Obtener la fecha actual
+const currentDate = new Date();
+
+// Obtener el número de días en el mes actual
+const numDays = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+
+// Obtener el nombre del mes actual
+const monthName = new Intl.DateTimeFormat("es-ES", { month: "long" }).format(currentDate);
+
+// Capitalizar la primera letra del nombre del mes
+const capitalizedMonth = monthName.charAt(0).toUpperCase() + monthName.slice(1);
+
+// Establecer el nombre del mes en los elementos correspondientes
+monthElement.textContent = capitalizedMonth;
+
+// Obtener el año actual
+const year = currentDate.getFullYear();
+
+// Establecer el nombre del mes y el año en los elementos correspondientes
+monthElement.textContent = monthName;
+yearElement.textContent = year;
+
+// Obtener el primer día de la semana del mes actual
+const firstDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1).getDay();
+
+// Obtener la referencia al elemento con la clase "dates"
+const datesContainer = document.querySelector(".dates");
+
+// Generar los números de los días y agregarlos al contenedor
+for (let i = 1; i <= numDays; i++) {
+  const dateElement = document.createElement("div");
+  dateElement.classList.add("date");
+  dateElement.textContent = i;
+  datesContainer.appendChild(dateElement);
+  
+// Si el día actual es el primer día del mes, aplicar el margen izquierdo correspondiente
+if (i === 1) {
+  dateElement.style.gridColumnStart = firstDay + 1;
+}
+const eventDate = 25; // Día con evento (ejemplo)
+if (i === eventDate) {
+  dateElement.classList.add("event");
+  const eventTooltip = document.createElement("div");
+  eventTooltip.classList.add("event-tooltip");
+  eventTooltip.textContent = "Acto por el 25 de mayo";
+  dateElement.appendChild(eventTooltip);
+}
+}
